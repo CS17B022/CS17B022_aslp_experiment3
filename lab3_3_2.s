8470293cdc2d567a62a9f741ab7fa0926d3604b9
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-02-17 21:43:55 +0530

    Update lab3_3_2.s

--- lab3_3_2.s
+++ lab3_3_2.s
@@ -5,7 +5,7 @@ li $s1,8	#b
 
 while:
 beq $s1,$t0,Exit
-srl $s0,$s0,1 #shift 'a' 3 times for division with 8
+srl $s0,$s0,1 #shift 'a' 3 times for division with 8 #using immediate addressing mode
 srl $s1,$s1,1 #shift 'b' 3 times to make it 1 from 8
 j while
 
