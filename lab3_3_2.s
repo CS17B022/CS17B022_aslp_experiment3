b5e6d3c3e52f90e0f89207881b5dc20649a4840e
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-02-17 21:25:56 +0530

    Update lab3_3_2.s

--- lab3_3_2.s
+++ lab3_3_2.s
@@ -1,13 +1,12 @@
 main:
 li $t0,1
-li $t1,2
-li $s0,16 	#a
+li $s0,16 	#a #final result in $s0
 li $s1,8	#b
 
 while:
 beq $s1,$t0,Exit
-srl $s0,$s0,1
-srl $s1,$s1,1
+srl $s0,$s0,1 #shift 'a' 3 times for division with 8
+srl $s1,$s1,1 #shift 'b' 3 times to make it 1 from 8
 j while
 
 Exit:
