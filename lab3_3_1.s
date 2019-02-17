f1b3d1c0922f52408c64de73071dd20d68d5448c
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-02-17 21:43:27 +0530

    Update lab3_3_1.s

--- lab3_3_1.s
+++ lab3_3_1.s
@@ -1,11 +1,11 @@
-main:
+main: 
 li $t0,1
 li $s0,4 	#a #final result in $s0
 li $s1,8	#b
 
 while:
 beq $s1,$t0,Exit
-sll $s0,$s0,1 #shift 'a' left 3 times for multiplication with 8
+sll $s0,$s0,1 #shift 'a' left 3 times for multiplication with 8 #using immediate addressing mode
 srl $s1,$s1,1 #shift 'b' right 3 times to make it 1 from 8
 j while
 
