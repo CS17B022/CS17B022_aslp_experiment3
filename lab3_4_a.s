f6cde05f0add7b5e800e29a11631d85e16f85426
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-02-18 01:25:47 +0530

    Update lab3_4_a.s

--- lab3_4_a.s
+++ lab3_4_a.s
@@ -6,3 +6,5 @@ or $s3,$s0,$s1	#or of numbers stored in s3
 xor $s4,$s0,$s1 #xor of numbers stored in s4
 li $t0,-1	#making all 32 bits of t0 equal to 1
 xor $s5,$s4,$t0	#taking xor with -1 gives not of the expression #xnor of numbers stored in s5
+jr $ra
+.end main
