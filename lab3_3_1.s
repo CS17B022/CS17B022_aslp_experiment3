242a3a754ae17c003d1e27a258604421827f5a30
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-02-17 21:15:05 +0530

    Update lab3_3_1.s

--- lab3_3_1.s
+++ lab3_3_1.s
@@ -6,8 +6,8 @@ li $s1,8	#b
 
 while:
 beq $s1,$t0,Exit
-sll $s0,$s0,1
-srl $s1,$s1,1
+sll $s0,$s0,1 #shift 'a' left 3 times for multiplication with 8
+srl $s1,$s1,1 #shift 'b' right 3 times to make it 1 from 8
 j while
 
 Exit:
